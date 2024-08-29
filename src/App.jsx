ab68dace0e8f5dca3834ac500eea8b38fb142dd2
Servet Isikli <servetisikli@gmail.com>
2024-08-29 13:44:43 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>React Test Deployment</h1>
+      <h1>Test Deployment</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
